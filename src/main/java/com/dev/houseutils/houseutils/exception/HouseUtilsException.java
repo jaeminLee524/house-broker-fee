01c03476abca980b4fe2cd51eeacac60601cae08
이재민 <jmlee@commerce-league.com>
2022-10-30 16:30:47 +0900

    로직 수정

--- src/main/java/com/dev/houseutils/houseutils/exception/HouseUtilsException.java
+++ src/main/java/com/dev/houseutils/houseutils/exception/HouseUtilsException.java
@@ -1,8 +1,16 @@
 package com.dev.houseutils.houseutils.exception;
 
 public class HouseUtilsException extends RuntimeException {
+    private ErrorCode errorCode;
+    private String message;
 
     public HouseUtilsException(ErrorCode errorCode) {
-        super(errorCode.getDescription());
+        this(errorCode, errorCode.getMessage());
+    }
+
+    public HouseUtilsException(ErrorCode errorCode, String customMessage) {
+        super(customMessage);
+        this.errorCode = errorCode;
+        this.message = customMessage;
     }
 }
--- src/main/java/com/dev/houseutils/houseutils/policy/BrokeragePolicyFactory.java
+++ src/main/java/com/dev/houseutils/houseutils/policy/BrokeragePolicyFactory.java
@@ -1,6 +1,8 @@
 package com.dev.houseutils.houseutils.policy;
 
 import com.dev.houseutils.houseutils.constants.ActionType;
+import com.dev.houseutils.houseutils.exception.ErrorCode;
+import com.dev.houseutils.houseutils.exception.HouseUtilsException;
 
 /**
  * 거래 타입에 따른 생성자 반환
@@ -18,7 +20,7 @@ public class BrokeragePolicyFactory {
             case RENT:
                 return new RentBrokeragePolicy();
             default:
-                throw new IllegalArgumentException(String.format("해당 %s에 대한 정책이 존재하지 않습니다.", type));
+                throw new HouseUtilsException(ErrorCode.INVALID_REQUEST, String.format("해당 %s에 대한 정책이 존재하지 않습니다.", type));
         }
     }
 }
